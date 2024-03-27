96c77ce809c7ef1044cec725f0c2843aa8c9eb68
bipro <biprocuet@gmail.com>
2024-03-27 23:43:22 +0600

    added video

--- src/app/component/CardVideoSection.jsx
+++ src/app/component/CardVideoSection.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+// import car from "../../../resources/car.mp4"
 function CardVideoSection() {
   return (
-    <div>CardVideoSection</div>
-  )
+    <div className="mt-32">
+      <h1 className="text-5xl font-body my-10 text-center">
+        Bangladesh's most ambition car
+      </h1>
+      <div>
+        <video
+        autoPlay loop controls
+          style={{ width: "100%", height: "100%" }}
+          src="../../../resources/car.mp4"
+        >
+
+        </video>
+      </div>
+    </div>
+  );
 }
 
-export default CardVideoSection
+export default CardVideoSection;
