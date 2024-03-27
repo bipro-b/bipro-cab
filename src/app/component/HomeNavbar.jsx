ea254b4184e9f25356b770dac4eac383a55b6636
bipro <biprocuet@gmail.com>
2024-03-27 19:29:24 +0600

    dummy nav bar designed

--- src/app/component/HomeNavbar.jsx
+++ src/app/component/HomeNavbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'
+import React from "react";
+
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 function HomeNavbar() {
   return (
-    <div>HomeNavbar</div>
-  )
+    <nav className="bg-black py-4">
+      <div className="container max-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <div className="flex items-center mx-5">
+              <img src="" alt="logo" />
+            </div>
+            <div>
+              <ul className="flex items-center">
+                <li className="mr-6">
+                  <a href="#" className="text-white hover:text-blue-200">
+                    ola S1
+                  </a>
+                </li>
+                <li className="mr-6">
+                  <a href="#" className="text-white hover:text-blue-200">
+                    pla electric
+                  </a>
+                </li>
+                <li className="mr-6">
+                  <a href="#" className="text-white hover:text-blue-200">
+                    ola futureFactory
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+
+            <button className="bg-gray-700 text-white hover:bg-gray-900 text-sm font-semibold px-4 py-3">
+              Book an Ola Cab
+            </button>
+            <button className="bg-white text-black hover:bg-gray-900 text-sm font-semibold px-4 py-3 ml-5">
+              Free S1 Test Driving
+            </button>
+
+            <MenuIcon className="ml-5 text-white text-3xl" />
+         
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default HomeNavbar
+export default HomeNavbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import HomeNavbar from "./component/HomeNavbar";
+import Banner from "./component/Banner";
+import CardVideoSection from "./component/CardVideoSection";
+import Footer from "./component/Footer";
 
 export default function Home() {
   return (
     <main className="">
-    Hello Ola cab
+      <HomeNavbar />
+      <Banner />
+      <CardVideoSection />
+      <Footer />
     </main>
   );
 }
